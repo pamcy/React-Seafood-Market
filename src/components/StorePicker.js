38ea8047f7052c85b9ad2bbe61ea15463dc6d2ab
Pamcy <chinyi.pam@gmail.com>
2019-05-19 18:36:03 +0800

    :sparkles: Add component 'StorePicker'

---
+++ src/components/StorePicker.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <p>Dummy p tag for demo purpose only</p>
+        <form action="" className="store-selector">
+          {/* this is a demo comment */}
+          <h2>Please Enter A Store</h2>
+          <input type="text" placeholder="Enter a store name" required/>
+          <button type="submit">Visit store 👉</button>
+        </form>
+      </ React.Fragment>
+    )
+  }
+}
+
+export default StorePicker;
+
+// NOTE
+/*
+1. render() return 只能回傳一個 parent，如果要多個 children 必須用 <React.Fragment> 或 <></ >包起來
+
+via: React v16.2.0: Improved Support for Fragments
+https://reactjs.org/blog/2017/11/28/react-v16.2.0-fragment-support.html
+*/
+
+/*
+2. comment 也不能馬上放在 return 之後，會出現 error，因為 return之後只能回傳一個 parent
+*/
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react';
+// import { Component } from 'react'; // 也可以這樣寫
+import { render } from 'react-dom'; // {} means Cheery pick only  the render method from 'react-dom' package
+import StorePicker from './components/StorePicker';
+import './css/style.css'
+
+render(<StorePicker />, document.querySelector('#main'));
