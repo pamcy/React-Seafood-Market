f8a3f1332704d904b0d3a1843d5141a2ce8f3bc1
Pamcy <chinyi.pam@gmail.com>
2019-05-20 12:33:46 +0800

    Create layout with main components

---
+++ src/components/Inventory.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Inventory extends React.Component {
+  render() {
+    return <div className="inventory">I am INVENTORY</div>;
+  }
+}
+
+export default Inventory;
---
+++ src/components/Order.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Order extends React.Component {
+  render() {
+    return <div className="order">I am ORDER</div>;
+  }
+}
+
+export default Order;
