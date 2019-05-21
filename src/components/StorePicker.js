603212b2467a33c1bb3b9d20562659b37ddba972
Pamcy <chinyi.pam@gmail.com>
2019-05-22 07:15:57 +0800

    :sparkles: Display random store names

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
   render() {
@@ -8,7 +9,12 @@ class StorePicker extends React.Component {
         <form action="" className="store-selector">
           {/* this is a demo comment */}
           <h2>Please Enter A Store</h2>
-          <input type="text" placeholder="Enter a store name" required />
+          <input
+            type="text"
+            placeholder="Enter a store name"
+            defaultValue={getFunName()}
+            required
+          />
           <button type="submit">Visit store 👉</button>
         </form>
       </React.Fragment>
@@ -30,4 +36,10 @@ https://reactjs.org/blog/2017/11/28/react-v16.2.0-fragment-support.html
 
 /*
 2. comment 也不能馬上放在 return 之後，會出現 error，因為 return之後只能回傳一個 parent
+*/
+
+/*
+3. <input type="text" value={getFunName()} />
+"value" 專門用在和 state 綁定資料的時候。
+input 的預設顯示值要用 "deaultValue"，當 component mounts to the page，會執行 getFunName() 回傳商店名字；
 */
