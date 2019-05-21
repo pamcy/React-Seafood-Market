829ee783095621fde8e51bb719b59ac0ccfecca5
Pamcy <chinyi.pam@gmail.com>
2019-05-21 17:14:32 +0800

    :sparkles: Set up react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 // import { Component } from 'react'; // 也可以這樣寫
 import { render } from 'react-dom'; // {} means Cheery pick only the render method from 'react-dom' package
-import StorePicker from './components/StorePicker';
-import App from './components/App';
+import Router from './components/Router';
 import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+render(<Router />, document.querySelector('#main'));
