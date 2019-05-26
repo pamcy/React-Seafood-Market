59a4b9fc324ba729366d90322e25063d8ceb3edf
Pamcy <chinyi.pam@gmail.com>
2019-05-26 11:52:21 +0800

    :sparkles: Create addFishForm and set fishes state

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,23 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+  };
+
+  addFish = fish => {
+    // Take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+
+    // Give every fish a unique id, and add new fish into fishes
+    fishes[`fish${Date.now()}`] = fish;
+
+    // Set the new fishes object to state
+    this.setState({
+      fishes: fishes,
+    });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,10 +28,35 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+// NOTE
+/*
+1. state 也可以寫在 constructor 裡面
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+}
+*/
+
+/*
+2. state 一定要給個空的預設初始值，在畫面還沒 mount 前用
+state = {
+  fishes1 = "", // string
+  fishes2 = null, // number
+  fishes3 = [], // array
+  fishes4 = {}, // object
+}
+*/
