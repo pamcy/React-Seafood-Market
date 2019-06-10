abd35952977bfa196ee26ad061ff7bcc674f0c47
Pamcy <chinyi.pam@gmail.com>
2019-06-10 11:54:39 +0800

    :bug: Remove dotenv use built-in .env instead

--- src/base.js
+++ src/base.js
@@ -1,11 +1,10 @@
 import Rebase from 're-base';
 import firebase from 'firebase/app';
 import 'firebase/database';
-require('dotenv').config();
 
 // Initialize Firebase
 const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.FIREBASE_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
   authDomain: "react-seafood-dashboard.firebaseapp.com",
   databaseURL: "https://react-seafood-dashboard.firebaseio.com",
 });
